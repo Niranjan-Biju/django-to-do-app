79ea5cccc956e7229000f6fc18790fc0a1fdcd15
Niranjan-Biju <niranjansreyas123@gmail.com>
2025-07-21 22:54:21 +0530

    added user auth - basic django authentication

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.taskList,name='tasks'),
     path('update/<int:pk>/', views.updateTask, name='update-task'),
     path('delete/<int:pk>/', views.deleteTask, name='delete-task'),
     path('edit/<int:pk>/', views.editTask, name='edit-task'),
+    path('login/', auth_views.LoginView.as_view(template_name='registrations/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,23 +2,28 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 from django.views.decorators.http import require_POST  # required for POST-only view
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # 🏠 Task List View
+@login_required
 def taskList(request):
-    tasks = Task.objects.order_by('-created') 
+    tasks = Task.objects.filter(user=request.user).order_by('-created')
     form = TaskForm()
 
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('/')
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('tasks')
 
-    context = {'tasks': tasks, 'form': form}
-    return render(request, 'tasks/list.html', context)
+    return render(request, 'tasks/list.html', {'tasks': tasks, 'form': form})
 
 # ✅ Checkbox Toggle View
 @require_POST
+@login_required
 def updateTask(request, pk):
     task = get_object_or_404(Task, id=pk)
     task.completed = not task.completed
@@ -27,12 +32,14 @@ def updateTask(request, pk):
 
 # Delete Task View
 @require_POST
+@login_required
 def deleteTask(request, pk):
     task = get_object_or_404(Task, id=pk)
     task.delete()
     return redirect('/')
 
 #Edit Task View
+@login_required
 def editTask(request, pk):
     task = get_object_or_404(Task, id=pk)
 
@@ -45,3 +52,14 @@ def editTask(request, pk):
         form = TaskForm(instance=task)
     
     return render(request, 'tasks/edit.html', {'form': form, 'task': task})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task-list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
